adaa107cc9a632926f058e2f6d84c04a67a7074c
ROBERT <61718725+Robaledo11@users.noreply.github.com>
2020-07-31 01:45:01 -0700

    Update covidsite.rb

--- lib/covid_testing/covidsite.rb
+++ lib/covid_testing/covidsite.rb
@@ -4,16 +4,17 @@
 
 module CovidTesting
   class CovidSite 
-    @@all = []
+    
+    @@all = []  
 
-    def self.all
+    def self.all 
       @@all
     end
 
-    attr_accessor :id, :name, :review_count, :rating, :location, :display_phone
+    attr_accessor :id, :name, :review_count, :rating, :location, :display_phone 
 
     def initialize(attributes)
-      attributes.each do |attribute_name,attribute_value|
+      attributes.each do |attribute_name,attribute_value|   
         if self.respond_to?("#{attribute_name}=")
         self.send("#{attribute_name}=", attribute_value)
         end
@@ -21,10 +22,10 @@ module CovidTesting
     end
 
     def self.find_by_number(number)
-      self.all[number.to_i - 1]
+      CovidSite.all[number.to_i - 1]
     end
 
-    def self.load_by_location(location)                                  #called class method w/ covid sites string
+    def self.load_by_location(location)                                  
       location_results = API.yelp_search("COVID 19", location)
       @@all = self.search_results_from_collection(location_results)
     end
@@ -36,17 +37,17 @@ module CovidTesting
     end
       
 
-    def details
-      <<-HEREDOC
-      
-        #{self.name} has #{self.review_count} reviews with an average rating of #{self.rating}
-        #{self.address}
-        #{self.display_phone}
-      HEREDOC
-    end
-
     def address
       location["display_address"].join("\n")
     end
   end
 end
+
+
+
+
+
+
+
+
+
